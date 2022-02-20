ee7b4d93caa0a750d7cdbddb00d98d3b6c5aca6d
lexuzelEagle <oleksandr.orlov@uga.edu>
2022-02-19 23:37:54 -0500

    Lesson 54. Launcher. Spawn projectiles

--- Source/ShootThemUp/Private/Weapons/STULauncherWeapon.cpp
+++ Source/ShootThemUp/Private/Weapons/STULauncherWeapon.cpp
@@ -2,4 +2,38 @@
 
 
 #include "Weapons/STULauncherWeapon.h"
+#include "Weapons/STUProjectile.h"
+#include "Kismet/GameplayStatics.h"
 
+void ASTULauncherWeapon::FireStart()
+{
+	Super::FireStart();
+
+	MakeShot();
+}
+
+void ASTULauncherWeapon::FireStop()
+{
+	Super::FireStop();
+}
+
+void ASTULauncherWeapon::MakeShot()
+{
+	const FTransform SocketTransform = WeaponMesh->GetSocketTransform(MuzzleSocketName);
+
+	const FTransform SpawnTransform(FRotator::ZeroRotator, SocketTransform.GetLocation());
+	AActor* Projectile = UGameplayStatics::BeginDeferredActorSpawnFromClass(GetWorld(), ProjectileClass, SpawnTransform);
+	// set projectile params
+
+	UGameplayStatics::FinishSpawningActor(Projectile, SpawnTransform);
+}
+
+void ASTULauncherWeapon::DealDamage(const FHitResult& HitResult)
+{
+	Super::DealDamage(HitResult);
+
+	const auto Victim = HitResult.GetActor();
+	check(Victim);
+
+	Victim->TakeDamage(WeaponDamage, FDamageEvent(), nullptr, GetOwner());
+}
--- Source/ShootThemUp/Public/Weapons/STULauncherWeapon.h
+++ Source/ShootThemUp/Public/Weapons/STULauncherWeapon.h
@@ -6,12 +6,24 @@
 #include "Weapons/STUBaseWeapon.h"
 #include "STULauncherWeapon.generated.h"
 
-/**
- * 
- */
+class ASTUProjectile;
+
 UCLASS()
 class SHOOTTHEMUP_API ASTULauncherWeapon : public ASTUBaseWeapon
 {
 	GENERATED_BODY()
-	
+
+public:
+	virtual void FireStart() override;
+	virtual void FireStop() override;
+
+protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Weapon")
+		TSubclassOf<ASTUProjectile> ProjectileClass;
+
+	virtual void MakeShot() override;
+	virtual void DealDamage(const FHitResult& HitResult) override;
+
+private:
+
 };
